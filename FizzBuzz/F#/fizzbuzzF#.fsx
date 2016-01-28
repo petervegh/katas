89c0c149acb7230265cf0ddfa56744d2ac88e75b
peter vegh <peter.vegh@protonmail.ch>
2016-01-28 08:50:32 +0000

    fizzbuzz - cleanup + F# fizbuzz

---
+++ FizzBuzz/F#/fizzbuzzF#.fsx
@@ -0,0 +1,8 @@
+﻿let fizzBuzz input = 
+    match input with
+    | _ when input % 3 = 0 && input % 5 = 0 -> "fizzbuzz"
+    | _ when input % 3 = 0 -> "fizz"
+    | _ when input % 5 = 0 -> "buzz"
+    | _ -> input.ToString()
+
+[1 .. 100] |> List.map(fizzBuzz)
